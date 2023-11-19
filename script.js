b10e3a2114bd74f25042c87a3da216dcd762910c
PixelForgeStudio <142952582+MineCraftStudios1@users.noreply.github.com>
2023-11-19 19:55:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-    const canvas = document.getElementById('space');
-    const context = canvas.getContext('2d');
+const canvas = document.getElementById('space');
+const context = canvas.getContext('2d');
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    let stars = [];
-    const starCount = 1000;
+let stars = [];
+const starCount = 1000;
 
-    for (let i = 0; i < starCount; i++) {
-      stars.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: Math.random() * 2,
-        color: 'white',
-        speed: Math.random() * 2 + 0.1,
-      });
+for (let i = 0; i < starCount; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: 2, // 设置较大的半径
+    color: 'white',
+    speed: Math.random() * 2 + 0.1,
+    opacity: Math.random() // 设置随机透明度
+  });
+}
+
+function drawStars() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < starCount; i++) {
+    const star = stars[i];
+    context.beginPath();
+    context.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+    context.fillStyle = `rgba(255, 255, 255, ${star.opacity})`; // 使用随机透明度
+    context.fill();
+  }
+}
+
+function updateStars() {
+  for (let i = 0; i < starCount; i++) {
+    stars[i].y -= stars[i].speed;
+    if (stars[i].y < 0) {
+      stars[i].y = canvas.height;
+      stars[i].x = Math.random() * canvas.width;
     }
+  }
+}
 
-    function drawStars() {
-      context.fillStyle = '#000';
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      for (let i = 0; i < starCount; i++) {
-        const star = stars[i];
-        context.beginPath();
-        context.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-        context.fillStyle = star.color;
-        context.fill();
-      }
-    }
+function animate() {
+  drawStars();
+  updateStars();
+  requestAnimationFrame(animate);
+}
 
-    function updateStars() {
-      for (let i = 0; i < starCount; i++) {
-        stars[i].y -= stars[i].speed;
-        if (stars[i].y < 0) {
-          stars[i].y = canvas.height;
-          stars[i].x = Math.random() * canvas.width;
-        }
-      }
-    }
-
-    function animate() {
-      drawStars();
-      updateStars();
-      requestAnimationFrame(animate);
-    }
-
-    animate();
+animate();
