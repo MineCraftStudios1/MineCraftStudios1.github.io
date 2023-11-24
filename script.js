330364d06b58d116d14c405b00c4fd3135fbb425
PixelForgeStudio <142952582+MineCraftStudios1@users.noreply.github.com>
2023-11-24 21:27:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,33 +11,24 @@ for (let i = 0; i < starCount; i++) {
   stars.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    radius: 2, // 设置较大的半径
+    radius: 2,
     color: 'white',
     speed: Math.random() * 2 + 0.1,
-    opacity: Math.random() // 设置随机透明度
+    opacity: Math.random()
   });
 }
 
 function drawStars() {
-  const gradient = context.createLinearGradient(0, 0, 0, canvas.height);
-  gradient.addColorStop(0, '#6a5acd'); // 添加起始颜色
-  gradient.addColorStop(0.4, '#000033'); // 添加第一个中间颜色
-  gradient.addColorStop(0.6, '#000033'); // 添加第二个中间颜色
-  gradient.addColorStop(1, '#6a5acd'); // 添加结束颜色
-  context.fillStyle = gradient;
+  context.fillStyle = '#000000';
   context.fillRect(0, 0, canvas.width, canvas.height);
   
   for (let i = 0; i < starCount; i++) {
     const star = stars[i];
     context.beginPath();
     context.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-    context.fillStyle = `rgba(255, 255, 255, ${star.opacity})`; // 使用随机透明度
+    context.fillStyle = `rgba(255, 255, 255, ${star.opacity})`;
     context.fill();
   }
-  context.save()
-
-  context.fillStyle = "#000000";
-  context.fillRect(0, 0, canvas.width, 100);
 }
 
 function updateStars() {
@@ -50,17 +41,9 @@ function updateStars() {
   }
 }
 
-let colorStop = 0;
-
-function updateColorStop() {
-  colorStop += 0.001;
-  colorStop %= 1;
-}
-
 function animate() {
   drawStars();
   updateStars();
-  updateColorStop();
   requestAnimationFrame(animate);
 }
 
